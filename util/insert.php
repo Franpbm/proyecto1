401dbcace896a1b13a64b7fd541beb03684da4af
adrianhr97 <triatlon8@gmail.com>
2019-01-17 13:38:28 +0000

    insert hecho y select tambien

--- util/insert.php
+++ util/insert.php
@@ -15,17 +15,37 @@
                 $message = $_POST['message'];
                 $date = date("Y-m-d");
                 insertUserObject($user, $message, $date);
-                selectUserObject($user, $message, $date);
             }
-            
+
+            $info = getMsgs();
+            if ($info != null){
+                foreach ($info as $register){
+                    echo '<p><b>ID:</b> '.$register['id'].'</p>';
+                    echo '<p><b>NAME:</b> '.$register['user'].'</p>';
+                    echo '<p><b>MESSAGE:</b> '.$register['message'].'</p>';
+                    echo '<p><b>DATE:</b> '.$register['date'].'</p>';
+                    echo '<br>';
+
+                }
+            }
+            else{
+                echo '<p>NO MESSAGES TO SHOW</p>';
+
+            }     
     ?> 
 
 <div class="container">
   <form method="POST" action="#">
     <div class="form-group">
       <label for="genial">
+            <label>USER NAME: </label>
             <input type="text" name="user">
+            <br>
+            <br>
+            <label>USER MSG: </label>
             <input type="text" name="message">
+            <br>
+
             <button type="submit" name="submit">Probar</button>
       </label>
     </div>
