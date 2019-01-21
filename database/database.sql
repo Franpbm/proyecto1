717fb1df6cf4841a30bd4c084fb92ba7b453f97a
Tsasir2 <master135351@gmail.com>
2019-01-21 13:36:38 +0000

    base datos

--- database/database.sql
+++ database/database.sql
@@ -1,15 +1,27 @@
 CREATE DATABASE prueba;
 
-CREATE TABLE prueba.test(
-	id int(11) PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `user` (
+    id int(11) PRIMARY KEY AUTO_INCREMENT,
     name varchar(20) NOT NULL,
-    msg varchar(250) NOT NULL,
-    datetime datetime NOT NULL
+    surnames varchar(50) NOT NULL,
+    username varchar(20) NOT NULL,
+    birthdate date NOT NULL,
+    password varchar(100) NOT NULL,
+    passwordVerificate varchar(100) NOT NULL,
+    email varchar(100) NOT NULL
 );
 
--- Example messages
-INSERT INTO test (name, msg, datetime) VALUES (
-    "Juan", "Que risaaaa....", "2019-01-16 10:24:12"),
+CREATE TABLE `prueba.test`(
+	  id int(11) PRIMARY KEY AUTO_INCREMENT,
+    message_id int(11),
+    name varchar(20) NOT NULL,
+    msg varchar(250) NOT NULL,
+    datetime datetime NOT NULL,
+    FOREIGN KEY (message_id) REFERENCES `user`(id) ON UPDATE CASCADE
+);
+
+INSERT INTO test (name, msg, datetime) VALUES 
+    ("Juan", "Que risaaaa....", "2019-01-16 10:24:12"),
     ("Pedrito", "xd", "2019-01-16 01:12:00"),
     ("Maria", "relajense señores que la estan armando", "2019-01-16 00:48:31"),
     ("Juan", "Que risa tio....", "2019-01-14 21:32:06"),
